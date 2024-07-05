c3413f2a4a6f4b31ce2a78194619edcb914e3d29
Kenji Longid <58013636+kenmalik@users.noreply.github.com>
2024-07-05 10:07:29 -0700

    feat: add second section placeholder

--- app/page.tsx
+++ app/page.tsx
@@ -18,12 +18,23 @@ export default function Home() {
             }}
           />
           <div className="absolute flex h-full w-full items-center justify-center p-12">
-            <h1 className="text-center text-6xl font-bold text-white">
-              A cabin in Sagada
+            <h1 className="text-center text-6xl font-bold text-orange-400">
+              A cabin + restaurant in Sagada, Philippines
             </h1>
           </div>
         </section>
-        <div>More test content</div>
+        <section className="mx-16 my-16 flex flex-col items-center">
+          <h1 className="mb-8 text-4xl font-bold">What we do</h1>
+          <p className="mb-8 italic">Do you want...</p>
+          <ul className="mb-16">
+            <li>Accommodation?</li>
+            <li>Food?</li>
+            <li>Bonfire?</li>
+            <li>Tour arrangement?</li>
+            <li>A great experience?</li>
+          </ul>
+          <p className="text-2xl font-bold">We gotchu!</p>
+        </section>
       </main>
     </>
   );
