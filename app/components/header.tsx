61e02b2a1b4b020397dfa49be0226a0029ee511d
Kenji Longid <58013636+kenmalik@users.noreply.github.com>
2024-07-06 11:50:56 -0700

    refactor: add "Ginuwine" to logos

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -12,11 +12,11 @@ export default function Header({
       className={`${fixed ? 'fixed' : ''} ${transparent ? 'animate-fade-out' : 'animate-fade-in bg-white'} z-50 flex w-full items-center justify-between px-8 py-4 md:px-16`}
     >
       <Link href="/">
-        <div className="hidden font-seaweed text-3xl font-bold min-[425px]:block">
-          Tagpuan + Lolo Dad&apos;s
+        <div className="hidden font-seaweed text-3xl font-bold sm:block">
+          Ginuwine Tagpuan + Lolo Dad&apos;s
         </div>
-        <div className="font-seaweed text-3xl font-bold min-[425px]:hidden">
-          T + LD&apos;s
+        <div className="font-seaweed text-3xl font-bold sm:hidden">
+          GT + LD&apos;s
         </div>
       </Link>
       <ul className="flex gap-4">
