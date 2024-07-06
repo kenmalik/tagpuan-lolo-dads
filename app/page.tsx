0d9e69c727ff346a7a388356df517ec362d2d4da
Kenji Longid <58013636+kenmalik@users.noreply.github.com>
2024-07-05 22:57:43 -0700

    feat: add copy to home page

--- app/page.tsx
+++ app/page.tsx
@@ -45,18 +45,22 @@ export default function Home() {
         </section>
         <section
           id="about"
-          className="mx-8 mb-64 mt-24 flex flex-col items-center"
+          className="x-8 mb-64 mt-24 flex flex-col items-center text-center sm:mx-24 md:mx-36 lg:mx-56 xl:mx-72 2xl:mx-96 3xl:mx-128"
         >
-          <h1 className="mb-12 text-4xl font-bold md:text-5xl">What we do</h1>
-          <p className="mb-8 text-lg italic">Do you want...</p>
-          <ul className="mb-8 text-lg">
+          <h1 className="mb-16 text-4xl font-bold md:text-5xl">What we do</h1>
+          <p className="mb-2 text-lg italic">Do you want...</p>
+          <ul className="mb-16 text-lg">
             <li>Accommodation?</li>
-            <li>Food?</li>
-            <li>Bonfire?</li>
-            <li>Tour arrangement?</li>
+            <li>Delicious food?</li>
+            <li>A warm bonfire?</li>
+            <li>Local tour guide?</li>
             <li>A great experience?</li>
           </ul>
-          <p className="text-3xl font-bold md:text-4xl">We gotchu!</p>
+          <p className="mb-12 text-3xl font-bold md:text-4xl">We gotchu!</p>
+          <p className="md:text-1xl text-xl">
+            Just come visit our retreat in the mountains of the beautiful
+            Sagada, Mountain Province.
+          </p>
         </section>
         <section
           id="tagpuan"
@@ -72,9 +76,10 @@ export default function Home() {
           </div>
           <div className="sm:px-10 lg:px-20 2xl:px-36">
             <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut a,
-              ad suscipit iusto dignissimos animi, dolores et at quo temporibus
-              quia ullam recusandae maiores porro itaque! Vitae ex numquam cum!
+              Grab a bite to eat at Ginuwine Tagpuan! Our patio dining area
+              provides an intimate space for adventurers to relax and mingle.
+              Meals can range from classic Pinoy soul food to explorations by
+              our in-cabin chefs.
             </p>
           </div>
           <div className="mt-12 flex flex-col items-center gap-4">
@@ -82,7 +87,7 @@ export default function Home() {
               href="/about"
               className="border-2 border-orange-400 px-8 py-2 transition ease-in-out hover:border-orange-300 hover:text-orange-400"
             >
-              Grab a meal!
+              Dine with us!
             </Link>
           </div>
         </section>
@@ -100,14 +105,28 @@ export default function Home() {
               <img src="/images/lolo-dads/cabin-patio.jpg" />
             </Gallery>
           </div>
-          <div className="sm:px-10 lg:px-20 2xl:px-36">
+          <div className="mb-8 sm:px-10 lg:px-20 2xl:px-36">
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem
-              cum rem pariatur molestias nihil cupiditate omnis perferendis quam
-              voluptas voluptate modi suscipit, consequatur ullam, corrupti eum
-              voluptates officia. Nisi, quo.
+              Welcome to Lolo Dad's Cabin! Use it as a basecamp for your
+              mountain excursions. Our four double beds can sleep up to eight.
+              For the deep sleepers there's also a couch and plenty of floor
+              space.
             </p>
           </div>
+          <ul className="flex flex-col items-center justify-center gap-4 text-neutral-600 sm:flex-row">
+            <li className="flex items-center gap-4">
+              <img src="/icons/wifi-orange.svg" alt="Wifi symbol" />
+              Wifi available
+            </li>
+            <li className="hidden sm:block">|</li>
+            <li className="flex items-center gap-4">
+              <img src="/icons/bed-orange.svg" alt="Bed icon" />4 Beds
+            </li>
+            <li className="hidden sm:block">|</li>
+            <li className="flex items-center gap-4">
+              <img src="/icons/shower-orange.svg" alt="Showerhead icon" />1 Bath
+            </li>
+          </ul>
           <div className="mt-12 flex flex-col items-center gap-4">
             <Link
               href="/about"
